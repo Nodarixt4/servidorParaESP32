7b47658debb3157e91ecf613b6c5008bc3d2b63b
Gabriel Nodari <103973562+Nodarixt4@users.noreply.github.com>
2024-11-24 14:05:55 -0400

    v 0.1

---
+++ src/indext.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.get('/nome', (req, res) => {
+    const nome = req.query.nome || "visitante";
+    res.send(`Opa, bão ${nome}?`);   
+});
+
+app.listen(port,'0.0.0.0', () => {
+    console.log(`Rodando na porta ${port}`);
+});
