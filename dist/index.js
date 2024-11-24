6d8183dc13c6497aabe4eef9366112fdce783159
Gabriel Nodari <103973562+Nodarixt4@users.noreply.github.com>
2024-11-24 18:56:33 -0400

    v1.4

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
-app.get('/nome', (req, res) => {
-    const nome = req.query.nome || "visitante";
-    res.send(`Opa, bão ${nome}?`);
+app.get('/temperatura', (req, res) => {
+    const temperatura = req.query.temperatura || "erro";
+    res.send(`${temperatura}°C`);
 });
 app.listen(port, '0.0.0.0', () => {
     console.log(`Rodando na porta ${port}`);
