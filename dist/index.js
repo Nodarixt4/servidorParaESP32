b9f6d39c4556ba078353608b763ca2c5ad6a5e97
Gabriel Nodari <103973562+Nodarixt4@users.noreply.github.com>
2024-11-25 13:29:06 -0400

    v2.5

--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,14 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
 const app = express();
-app.use(bodyParser.json());
-// Defina explicitamente o tipo de `valorArmazenado`
-let valorArmazenado = null; // Pode armazenar um número ou `null`
-// Endpoint para armazenar o valor enviado pelo ESP32
-app.post('/armazenar', (req, res) => {
-    const { valor } = req.body;
-    if (typeof valor === 'number') {
-        valorArmazenado = valor;
-        console.log(`Valor armazenado: ${valorArmazenado}`);
-        res.status(200).send('Valor armazenado com sucesso');
-    }
-    else {
-        res.status(400).send('Formato de valor inválido');
-    }
+const port = 3000;
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
 });
-// Endpoint para obter o valor armazenado
 app.get('/temperatura', (req, res) => {
-    if (valorArmazenado !== null) {
-        res.status(200).send(valorArmazenado.toString());
-    }
-    else {
-        res.status(404).send('Nenhum valor armazenado');
-    }
+    const temperatura = req.query.temperatura || "erro";
+    res.send(`${temperatura}°C`);
 });
-// Inicializa o servidor na porta 3000
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Rodando na porta ${port}`);
 });
