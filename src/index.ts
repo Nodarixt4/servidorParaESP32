f57ebfb95c46a0f34af1ac812aebcecd9d28fe16
Gabriel Nodari <103973562+Nodarixt4@users.noreply.github.com>
2024-11-24 20:28:48 -0400

    v1.10

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,16 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-let temperaturaAtual = "erro";
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
-// Endpoint para o ESP enviar a temperatura
-app.get('/setTemperatura', (req, res) => {
-    const temperatura = String(req.query.temperatura || "erro");  // Garantir que seja uma string
-    temperaturaAtual = temperatura;  // Armazena a temperatura recebida
-    res.send(`Temperatura atualizada para: ${temperaturaAtual}°C`);
-});
-
-// Endpoint para o front-end obter a temperatura
 app.get('/temperatura', (req, res) => {
-    res.send(`${temperaturaAtual}°C`);  // Retorna a temperatura atual
+    const temperatura = req.query.temperatura || "erro";
+    res.send(`${temperatura}°C`);   
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port,'0.0.0.0', () => {
     console.log(`Rodando na porta ${port}`);
 });
